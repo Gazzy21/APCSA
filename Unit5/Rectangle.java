c58649a967cb253b00e0433b51b312e3da5da79d
Gazzy21 <nicholas221@icloud.com>
2024-11-21 10:20:12 -0500

    added rectangle and rectangle tester

--- Unit5/Rectangle.java
+++ Unit5/Rectangle.java
@@ -1,4 +1,25 @@
 package Unit5;
+
 public class Rectangle {
-    
+    private int length;
+    private int width;
+    private String color;
+
+    public Rectangle(String rColor, int rWidth, int rLength) {
+        color = rColor;
+        width = rWidth;
+        length = rLength;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
 }
--- Unit5/RectangleTester.java
+++ Unit5/RectangleTester.java
@@ -1,7 +1,12 @@
 package Unit5;
+
 public class RectangleTester {
-    public static void main (String[] args) {
-        Rectangle r1 = 
-        Rectangle r2 = 
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle("blue", 10, 5);
+        Rectangle r2 = new Rectangle("red", 5, 10);
+
+        String c = r1.getColor();
+        System.out.println("r1 color is: " + c);
+        System.out.println("r1 length is: " + r1.getLength());
     }
 }
