0e157c2b16dcdf88106c651f34cd4e171389fd14
Gazzy21 <nicholas221@icloud.com>
2024-12-19 11:39:39 -0500

    odds evens

---
+++ Unit6/OddAnEvensRunner.java
@@ -0,0 +1,16 @@
+public class OddAnEvensRunner
+{
+	public static void main( String args[] )
+	{
+		//add test cases
+
+		// create 3 arrays with the values
+		// 2 4 6 8 10 12 14
+		// 1 2 3 4 5 6 7 8 9
+		// 2 10 20 21 23 24 40 55 60 61
+
+		// call all the methods in OddsAndEvens class
+		// be sure to print out the results
+
+	}
+}
---
+++ Unit6/OddsAndEvens.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OddsAndEvens
+{
+	// this method returns how many elements in array are odd
+	//  when odd is true. When odd is false, it returns how many
+	//  elements are even.
+	private static int countEm(int[] array, boolean odd)
+	{
+		return 0;
+	}
+	
+	// use the method countEm in the next two methods
+	public static int[] getAllEvens(int[] array)
+	{
+		return null;
+	}
+
+	public static int[] getAllOdds(int[] array)
+	{
+		return null;		
+	}
+}
