d8af863763ff4ae93a63742f007fd89c00adb2af
Gazzy21 <nicholas221@icloud.com>
2025-01-29 10:45:18 -0500

    idk

--- Unit7/Inventory.java
+++ Unit7/Inventory.java
@@ -5,5 +5,48 @@ public class Inventory {
     private ArrayList<Item> inStock;
 
     public Inventory() {
+        inStock = new ArrayList<Item>();
+    }
+
+    public void addItem(Item item) {
+        inStock.add(item);
+    }
+
+    public void removeItem(String name) {
+        for(int i = 0; i < inStock.size(); i++) {
+            if (inStock.get(i).getName() == name) {
+                inStock.remove(i);
+            }
+        }
+    }
+    
+    public void removeItem() {
+        for(int i = 0; i < inStock.size(); i++) {
+            if(inStock.get(i).getQuantity() <= 0) {
+                inStock.remove(i);
+            }
+        }
+    }
+
+    public void updatePrice(String name, double percentChange) {
+        for(int i = 0; i < inStock.size(); i++) {
+            if (inStock.get(i).getName() == name) {
+                inStock.get(i).setPrice(inStock.get(i).getPrice() * (1 + percentChange));
+            }
+        }
+    }
+
+    public int largestQuantityIndex() {
+        int largest = 0;
+        for(int i = 0; i < inStock.size(); i++) {
+            if (inStock.get(i).getQuantity() > largest) {
+                largest = i;
+            }
+        }
+        return largest;
+    }
+
+    public ArrayList<Item> getInventoryList() {
+        for (int i = 0; i < )
     }
 }
--- Unit7/StoreInventoryTester.java
+++ Unit7/StoreInventoryTester.java
@@ -1,3 +1,28 @@
 public class StoreInventoryTester {
-    
+    public static void main(String[] args) {
+        Inventory storeInventory = new Inventory();
+
+        storeInventory.addItem(new Item("Apple", 1.00, 10));
+        storeInventory.addItem(new Item("Apple", 0.50, 20));
+        storeInventory.addItem(new Item("Orange", 0.80, 15));
+        storeInventory.addItem(new Item("Grapes", 2.00, 5));
+        storeInventory.addItem(new Item("Pineapple", 3.00, 70));
+        storeInventory.addItem(new Item("Mango", 1.50, 0));
+        storeInventory.addItem(new Item("Peach", 1.20, 8));
+        storeInventory.addItem(new Item("Strawberry", 2.50, 10));
+        storeInventory.addItem(new Item("Blueberry", 3.50, 6));
+        storeInventory.addItem(new Item("Watermelon", 4.00, 4));
+
+        System.out.println(storeInventory.getInventoryList());
+
+        storeInventory.removeItem("Apple");
+        storeInventory.removeItem();
+
+        storeInventory.updatePrice("Watermelon", 1.50 * 1.5);
+
+        int largestQuantityIndex = storeInventory.largestQuantityIndex();
+        System.out.println("Largest quantity index: " + largestQuantityIndex);
+
+        storeInventory.getInventoryList();
+    }
 }
